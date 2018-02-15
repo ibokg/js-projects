c28938ed0987b376f2907275119bd04c23ff0d21
Ibadulla <asadli.ibadulla@gmail.com>
2018-02-15 14:51:15 +0600

    Add new version with rgb and hex

--- js-time/script.js
+++ js-time/script.js
@@ -1,20 +1,26 @@
-function getTimeFormat() {
-  var data = new Date(); 
-  hh = data.getHours(), mm = data.getMinutes(), ss = data.getSeconds();
-  hh = (hh<10) ? '0' + hh : hh;
-  mm = (mm<10) ? '0' + mm : mm;
-  ss = (ss<10) ? '0' + ss : ss;
-  return hh + ':' + mm + ':' + ss;
-}
+var timeOutput = document.getElementById('time-output'),
+    rgbOutput = document.getElementById('rgb-output'),
+    hexOutput = document.getElementById('hex-output');
+var date, hh, mm, ss;
 
-function resultTime() {
-  dateOutput.innerHTML = getTimeFormat();
-}
+function getTime() {
+  date = new Date();
+  hh = date.getHours(),
+  mm = date.getMinutes(),
+  ss = date.getSeconds();
+};
 
 setInterval(function() {
-  resultTime();
-  var resultColor = '#' + hh + mm + ss;
-  document.body.style.background = resultColor;
-  bgOutput.style.color = resultColor;
-  bgOutput.innerHTML = resultColor;
+  getTime();
+  hh = parseInt(hh*11);
+  mm = parseInt(mm*4.3);
+  ss = parseInt(ss*4.3);
+  colorRgb = 'rgb(' + hh + ', ' + mm + ', ' + ss + ')';
+  colorHex = '#' + hh.toString(16) + mm.toString(16) + ss.toString(16);
+
+  timeOutput.innerHTML = date.toLocaleTimeString('en-US', { hour12: false });
+  rgbOutput.innerHTML = colorRgb;
+  hexOutput.innerHTML = colorHex;
+  
+  document.body.style.background = colorRgb;
 }, 1000);
